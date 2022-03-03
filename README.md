46c7ce0dc8bc1891795df9509649b48cb38ce9bf
ex.codes <kendinikertenkelebek@me.com>
2022-03-03 17:10:54 +0300

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+## GUI
+
+Best GUI design for VALORANT.
+
+Please feel free for feature requests or open issues.
---
+++ src/moltenCheat.cpp
@@ -0,0 +1,5 @@
+/*
+Valorant GUI Repository @MoltenCheat
+Visit: https://github.com/MoltenCheat/GUI
+Valorant Version: Unknown
+*/
